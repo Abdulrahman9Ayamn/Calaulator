9dfd7e4d0c8abcd3a1e32f61b657bd328040a0dc
Abdulrhman <body1999221@gmail.com>
2024-02-15 23:12:07 +0200

    Createed the main Project Structure

---
+++ src/main/java/com/mycompany/my_calaulator/My_Calaulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.my_calaulator;
+
+/**
+ *
+ * @author qwertet
+ */
+public class My_Calaulator {
+
+    public static void main(String[] args) {
+        new frmCalc().setVisible(true);
+    }
+}
